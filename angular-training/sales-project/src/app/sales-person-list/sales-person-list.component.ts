dc159ae3f5c23fbaa5b042832f194c74ad167ec5
justsaykk <kaikein.woo@gmail.com>
2022-10-14 15:28:52 +0800

    Completed first part of sales person project

---
+++ angular-training/sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list.component.html',
+  styleUrls: ['./sales-person-list.component.css']
+})
+export class SalesPersonListComponent implements OnInit {
+
+  // Create Array of Objects
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson("KK", "Woo", "kkwoo@sales.com", 1000),
+    new SalesPerson("John", "Doe", "jd@sales.com", 2000),
+    new SalesPerson("Claire", "St.", "sclaire@sales.com", 3000),
+    new SalesPerson("May", "Throng", "emtee@sales.com", 4000),
+
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
